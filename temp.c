077331f3e99542e0e2ff185cba8ac3ad1abef52b
Luís Fellype <luisfellypemagalhaesgato@gmail.com>
2025-10-20 11:02:41 -0300

    temp.c update

--- temp.c
+++ temp.c
@@ -306,8 +306,8 @@ void setleft(ARV_BIN_SEQ *t, int p, int x){
     int ind = getNode(t);
     if (ind != -1){
         t->nodes[p].left = ind;
-        t->nodes[ind].info = x;
         t->nodes[ind].father = p;
+        t->nodes[ind].info = x;
         t->nodes[ind].left = -1;
         t->nodes[ind].right = -1;
     } else {
@@ -368,4 +368,84 @@ int isright(ARV_BIN_SEQ *t, int p){
     return 0;
 }
 
+// ÁRVORE BINÁRIA ENCADEADA
 
+typedef struct node {
+    int info;
+    struct node *left;
+    struct node *right;
+    struct node *father;
+} NODE;
+
+typedef NODE * ARV_BIN_ENC;
+
+void maketree(ARV_BIN_ENC *t, int x){
+    *t = (NODE*) malloc(sizeof(NODE));
+    if (!*t)
+        return;
+    (*t)->info = x;
+    (*t)->father = NULL;
+    (*t)->left = NULL;
+    (*t)->right = NULL;
+}
+
+void setleft(ARV_BIN_ENC t, int x){
+    t->left = (NODE*) malloc(sizeof(NODE));
+    if (!t->left)
+        return;
+    t->left->info = x;
+    t->left->father = t;
+    t->left->left = NULL;
+    t->left->right = NULL;
+}
+
+void setright(ARV_BIN_ENC t, int x){
+    t->right = (NODE*) malloc(sizeof(NODE));
+    if (!t->right)
+        return;
+    t->right->info = x;
+    t->right->father = t;
+    t->right->left = NULL;
+    t->right->right = NULL;
+}
+
+int info(ARV_BIN_ENC t){
+    return t->info;
+}
+
+ARV_BIN_ENC left(ARV_BIN_ENC t){
+    return t->left;
+}
+
+ARV_BIN_ENC right(ARV_BIN_ENC t){
+    return t->right;
+}
+
+ARV_BIN_ENC father(ARV_BIN_ENC t){
+    return t->father;
+}
+
+ARV_BIN_ENC brother(ARV_BIN_ENC t){
+    if (t->father)
+        if (isleft(t))
+            return t->father->right;
+        else
+            return t->father->left;
+    else
+        return NULL;
+}
+
+int isleft(ARV_BIN_ENC t){
+    NODE *q = t->father;
+    if (!q)
+        return 0;
+    if (q->left == t)
+        return 1;
+    return 0;
+}
+
+int isright(ARV_BIN_ENC t){
+    if (t->father)
+        return !isleft(t);
+    return 0;
+}
